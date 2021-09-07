94c771b56f6c9687b8981a6bf10fc3f174acee5d
郑为中 <916077357@qq.com>
2021-09-07 15:24:07 +0800

    增加Excel学习笔记

---
+++ excel/excel-poi/src/main/java/cn/zwz/PoiHssfTest.java
@@ -0,0 +1,67 @@
+package cn.zwz;
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.*;
+import org.junit.Test;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class PoiHssfTest {
+
+    private final static String PATH = "D:\\";
+
+    @Test
+    public void poiHssfWrite() throws IOException {
+        // 创建Excel
+        Workbook workbook = new HSSFWorkbook();
+        // 创建Sheet
+        Sheet sheet = workbook.createSheet("花名册档案");
+        // 创建行
+        Row row = sheet.createRow(0);
+        // 创建单元格
+        Cell cell1 = row.createCell(0);
+        Cell cell2 = row.createCell(1);
+        Cell cell3 = row.createCell(2);
+        Cell cell4 = row.createCell(3);
+        // 写值
+        cell1.setCellValue("姓名");
+        cell2.setCellValue("工号");
+        cell3.setCellValue("性别");
+        cell4.setCellValue("地址");
+        for(int i = 1 ; i <= 65535 ; i ++) {
+            // 再插入一行数据
+            row = sheet.createRow(i);
+            cell1 = row.createCell(0);
+            cell2 = row.createCell(1);
+            cell3 = row.createCell(2);
+            cell4 = row.createCell(3);
+            cell1.setCellValue("员工" + i);
+            cell2.setCellValue("ZWZ" + i);
+            cell3.setCellValue(i % 2 == 1? "男" : "女");
+            cell4.setCellValue("测试地址" + i);
+        }
+        // 生成文件
+        FileOutputStream fileOutputStream = new FileOutputStream(PATH + "test.xls");
+        workbook.write(fileOutputStream);
+        fileOutputStream.close();
+        System.out.println("OK");
+    }
+
+    @Test
+    public void poiHssfRead() throws IOException {
+        FileInputStream inputStream = new FileInputStream(PATH + "test.xls");
+        Workbook workbook = new HSSFWorkbook(inputStream);
+        Sheet sheet = workbook.getSheetAt(0);
+        for(int i = sheet.getFirstRowNum(); i <= sheet.getLastRowNum(); i ++) {
+            Row row = sheet.getRow(i);
+            for(int j = row.getFirstCellNum(); j <= row.getLastCellNum() - 1; j ++) {
+                // STRING 字符串 / NUMERIC 数字 / BOOLEAN 布尔 / BLANK 空
+                String cellType = row.getCell(j).getCellType().name();
+                System.out.print("[" + cellType + "]" + row.getCell(j) + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ excel/excel-poi/src/main/java/cn/zwz/ZwzApplication.java
@@ -0,0 +1,15 @@
+package cn.zwz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 郑为中
+ */
+@SpringBootApplication
+public class ZwzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZwzApplication.class, args);
+    }
+}
